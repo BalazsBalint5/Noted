5395ddcd7badf2a834f5b10412d059289231325b
Bálint <balazs.balint5@gmail.cpm>
2024-07-21 10:21:59 +0200

    feat: Add new interface for Note service. This interface specifies methods that a class working with Notes must implement

--- src/main/java/com/bb/noted/service/NoteService.java
+++ src/main/java/com/bb/noted/service/NoteService.java
@@ -1,2 +1,14 @@
-package com.bb.noted.service;public interface NoteService {
+package com.bb.noted.service;
+
+import com.bb.noted.domain.Note;
+
+import java.util.List;
+
+public interface NoteService {
+
+    Note getById(Integer id);
+    List<Note> getAll();
+    void addNote(Note note);
+    void deleteNote(Note note);
+    void deleteNoteById(Integer id);
 }
