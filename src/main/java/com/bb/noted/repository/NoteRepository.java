ba988a333e94cab963d5f2d9fdf6e7539e4f730d
Bálint <balazs.balint5@gmail.cpm>
2024-07-21 09:10:19 +0200

    feat: Add new note reposítory class

--- src/main/java/com/bb/noted/repository/NoteRepository.java
+++ src/main/java/com/bb/noted/repository/NoteRepository.java
@@ -1,2 +1,7 @@
-package com.bb.noted.repository;public class NoteRepository {
+package com.bb.noted.repository;
+
+import com.bb.noted.domain.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NoteRepository extends JpaRepository<Note, Integer> {
 }
